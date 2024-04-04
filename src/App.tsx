7439cbbf628f235b16a2e655a7fb687c591a9b5b
miquelven <miquelven.silva@gmail.com>
2024-04-03 22:18:15 -0300

    feat: consuming api to show posters

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react";
+import useGetMovie from "./hooks/useGetMovie";
+
+const baseImgUrl = "https://image.tmdb.org/t/p";
+const size = "w200";
+
 function App() {
+  const { data, isPending } = useGetMovie();
+  const [movieData, setMovieData] = useState([]);
+
+  useEffect(() => {
+    if (data?.results) {
+      setMovieData(data.results);
+      console.log(data);
+    }
+  }, [data]);
+
   return (
     <>
-      <div>hello</div>
+      {isPending && <>Carregando</>}
+      {!isPending && (
+        <>
+          {movieData.map((movie) => (
+            <img src={`${baseImgUrl}/${size}${movie["poster_path"]}`} />
+          ))}
+        </>
+      )}
     </>
   );
 }
--- src/hooks/useGetMovie.tsx
+++ src/hooks/useGetMovie.tsx
@@ -1,6 +1,29 @@
 import { useQuery } from "@tanstack/react-query";
 
-// const useGetMovie = useQuery({
-//     queryKey: ['movie-data'],
-//     queryFn:
-// })
+import axios from "axios";
+
+const value = import.meta.env.VITE_API_KEY;
+
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${value}`,
+  },
+};
+
+const getMovieData = async () => {
+  const data = await axios.get(
+    "https://api.themoviedb.org/3/movie/popular?language=pt-BR&page=1",
+    options
+  );
+  return data.data;
+};
+
+const useGetMovie = () =>
+  useQuery({
+    queryKey: ["movie-data"],
+    queryFn: getMovieData,
+  });
+
+export default useGetMovie;
