fa252cdc522cbaec729776af91c5d9874f45de62
miquelven <miquelven.silva@gmail.com>
2024-04-08 22:24:57 -0300

    style: improve responsiveness

--- src/components/VideosArea/index.tsx
+++ src/components/VideosArea/index.tsx
@@ -3,11 +3,25 @@ import CarouselTrailers from "./CarouselTrailers/index.tsx";
 
 export default function VideosArea() {
   return (
-    <section className="pt-20 pb-44 mb-20 w-full bg-[#0a0d14]">
-      <Container maxW="1580px" px="10px" position="relative">
-        <Flex justifyContent={"center"} gap="120px">
+    <section className="pt-20 pb-44 mb-20 w-full bg-[#0a0d14] max-sm:pb-0 max-sm:pt-28">
+      <Container
+        maxW="1580px"
+        px={{ base: "30px", lg: "10px" }}
+        position="relative"
+      >
+        <Flex
+          justifyContent={"center"}
+          gap={{ base: "60px", lg: "120px" }}
+          mb="60px"
+          flexDir={{ base: "column", lg: "row" }}
+        >
           <Heading as="h2">Trailers</Heading>
-          <Text maxW="800px" fontSize={"lg"} textAlign={"justify"}>
+          <Text
+            maxW="800px"
+            color="#fff9"
+            fontSize={{ base: "sm", sm: "md", lg: "lg" }}
+            textAlign={"justify"}
+          >
             Desfrute de uma prévia cinematográfica de alguns dos filmes mais
             cativantes em nossa coleção. Prepare-se para uma experiência visual
             única enquanto você navega pelos trailers selecionados especialmente
