48271498535469e264c371eaaa2abd2752107e47
miquelven <miquelven.silva@gmail.com>
2024-04-08 22:25:20 -0300

    feat: add carousel trailer

--- src/components/VideosArea/CarouselTrailers/index.tsx
+++ src/components/VideosArea/CarouselTrailers/index.tsx
@@ -1,3 +1,53 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import "./style.css";
+
+// import required modules
+import { EffectFlip, Navigation } from "swiper/modules";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+
+import "./style.css";
+import useGetTrailers from "../../../hooks/useGetTrailers";
+
 export default function CarouselTrailers() {
-  return <div>sdf</div>;
+  const { data: keys, isPending } = useGetTrailers("home");
+
+  return (
+    <>
+      <Swiper
+        centeredSlides={true}
+        slidesPerView={"auto"}
+        navigation={true}
+        modules={[EffectFlip, Navigation]}
+      >
+        {!isPending &&
+          keys?.map((key) => (
+            <SwiperSlide key={key}>
+              <iframe
+                width="100%"
+                height="100%"
+                src={`https://www.youtube.com/embed/${key}`}
+                title="YouTube video player"
+                allow="accelerometer; 
+                    autoplay; 
+                    clipboard-write; 
+                    encrypted-media; 
+                    gyroscope; 
+                    picture-in-picture; 
+                    web-share"
+              ></iframe>
+            </SwiperSlide>
+          ))}
+      </Swiper>
+    </>
+  );
 }
