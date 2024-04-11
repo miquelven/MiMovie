96993b519dde575d86b5ef1cff645afffa7110c4
miquelven <miquelven.silva@gmail.com>
2024-04-11 15:32:40 -0300

    feat: add button to top page

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -3,15 +3,57 @@ import LatestMovies from "../components/LatestMovies/index.tsx";
 import VideosArea from "../components/VideosArea/index.tsx";
 import FeaturesArea from "../components/FeaturesArea/index.tsx";
 import PopularMovies from "../components/PopularMovies/index.tsx";
+import { IconButton } from "@chakra-ui/react";
+import { FaArrowCircleUp } from "react-icons/fa";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [showScroll, setShowScroll] = useState(false);
+
+  useEffect(() => {
+    const checkScrollTop = () => {
+      if (!showScroll && window.pageYOffset > 800) {
+        setShowScroll(true);
+      } else if (showScroll && window.pageYOffset <= 800) {
+        setShowScroll(false);
+      }
+    };
+
+    window.addEventListener("scroll", checkScrollTop);
+
+    return () => {
+      window.removeEventListener("scroll", checkScrollTop);
+    };
+  }, [showScroll]);
+
+  const scrollTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
-    <div>
+    <main className="relative">
       <Banner />
       <VideosArea />
       <LatestMovies />
       <FeaturesArea />
       <PopularMovies />
-    </div>
+
+      <IconButton
+        onClick={scrollTop}
+        fontSize="36px"
+        colorScheme="transparent"
+        aria-label="To Top"
+        color="#fff9"
+        position="fixed"
+        bottom="32px"
+        right="32px"
+        style={{
+          transition: "all ease 500ms",
+          opacity: showScroll ? "1" : "0",
+          zIndex: showScroll ? "20" : "-1",
+        }}
+        icon={<FaArrowCircleUp />}
+      />
+    </main>
   );
 }
