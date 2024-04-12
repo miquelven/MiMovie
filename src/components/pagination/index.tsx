52eb511918583967815d17e2370cdc288e586fc4
miquelven <miquelven.silva@gmail.com>
2024-04-12 16:45:49 -0300

    style: change position element

--- src/components/pagination/index.tsx
+++ src/components/pagination/index.tsx
@@ -1,3 +1,4 @@
+import { Box, Center, Flex } from "@chakra-ui/react";
 import CurrentPagintaionButton from "./CurrentPaginationButton";
 import PaginationButton from "./PaginationButton";
 
@@ -11,26 +12,32 @@ interface infosType {
 
 export default function Pagination({ infos }: infosType) {
   return (
-    <div>
-      {infos.pageSelected > 1 && (
-        <>
-          <PaginationButton infos={{ ...infos, type: "prev" }}>
-            Anterior
-          </PaginationButton>
-          <PaginationButton infos={{ ...infos, type: "prev" }}>
-            {infos.pageSelected - 1}
-          </PaginationButton>
-        </>
-      )}
-      <CurrentPagintaionButton>{infos.pageSelected}</CurrentPagintaionButton>
-      {infos.pageSelected < 500 && (
-        <>
-          <PaginationButton infos={infos}>
-            {infos.pageSelected + 1}
-          </PaginationButton>
-          <PaginationButton infos={infos}>Próximo</PaginationButton>
-        </>
-      )}
-    </div>
+    <Box position={"absolute"} bottom={"-80px"} left="50%" right="50%">
+      <Center>
+        <Flex gap="24px" fontSize={{ base: "medium", sm: "large" }}>
+          {infos.pageSelected > 1 && (
+            <>
+              <PaginationButton infos={{ ...infos, type: "prev" }}>
+                Anterior
+              </PaginationButton>
+              <PaginationButton infos={{ ...infos, type: "prev" }}>
+                {infos.pageSelected - 1}
+              </PaginationButton>
+            </>
+          )}
+          <CurrentPagintaionButton>
+            {infos.pageSelected}
+          </CurrentPagintaionButton>
+          {infos.pageSelected < 500 && (
+            <>
+              <PaginationButton infos={infos}>
+                {infos.pageSelected + 1}
+              </PaginationButton>
+              <PaginationButton infos={infos}>Próximo</PaginationButton>
+            </>
+          )}
+        </Flex>
+      </Center>
+    </Box>
   );
 }
