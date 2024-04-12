749062cd45bbaa1b569f850c89b934ec27b4a2db
miquelven <miquelven.silva@gmail.com>
2024-04-12 16:43:51 -0300

    feat: add new route

--- src/main.tsx
+++ src/main.tsx
@@ -8,12 +8,16 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 // react router
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./routes/Home.tsx";
+import Popular from "./routes/Popular.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [{ path: "/", element: <Home /> }],
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "populares", element: <Popular /> },
+    ],
   },
 ]);
 
