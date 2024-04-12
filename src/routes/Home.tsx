8c8ae2638d2c3779b6776c034ba41f64abf405eb
miquelven <miquelven.silva@gmail.com>
2024-04-12 10:01:39 -0300

    fix: comment for deploy

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,6 +1,6 @@
-import Banner from "../components/Banner/index.tsx";
-import LatestMovies from "../components/LatestMovies/index.tsx";
-import VideosArea from "../components/VideosArea/index.tsx";
+// import Banner from "../components/Banner/index.tsx";
+// import LatestMovies from "../components/LatestMovies/index.tsx";
+// import VideosArea from "../components/VideosArea/index.tsx";
 import FeaturesArea from "../components/FeaturesArea/index.tsx";
 import PopularMovies from "../components/PopularMovies/index.tsx";
 import { IconButton } from "@chakra-ui/react";
