ffcd74de6683bfc32b3563714ce046372d7101bb
miquelven <miquelven.silva@gmail.com>
2024-04-16 14:31:41 -0300

    feat: add listcategoriesitem component

---
+++ src/components/ListCategories/index.tsx
@@ -0,0 +1,50 @@
+import { Box, Grid, GridItem } from "@chakra-ui/react";
+import useGetAllGenres from "../../hooks/useGetAllGenres";
+
+import { categoryData } from "../../data/categoryData";
+import { Link } from "react-router-dom";
+import ListCategoriesItem from "./ListCategoriesItem";
+
+interface genreType {
+  id: number;
+  name: string;
+}
+
+export default function ListCategories() {
+  const { data: allGenres, isPending } = useGetAllGenres();
+
+  return (
+    <Box mt="60px">
+      <Grid
+        templateColumns={{
+          base: "repeat(1, 300px)",
+          sm: "repeat(2,250px)",
+          md: "repeat(3,1fr)",
+        }}
+        rowGap={{ base: "50px", sm: "60p", lg: "80px" }}
+        justifyContent={"center"}
+        alignItems={"center"}
+      >
+        {!isPending &&
+          allGenres.genres.map((genre: genreType) => (
+            <GridItem
+              key={genre.id}
+              className="transition-all duration-700 hover:brightness-[.4]"
+            >
+              {categoryData.map(
+                (category) =>
+                  category.genreName == genre.name && (
+                    <Link
+                      to={`/categoria/${genre.name.split(" ").join("-")}`}
+                      key={category.genreName}
+                    >
+                      <ListCategoriesItem category={category} />
+                    </Link>
+                  )
+              )}
+            </GridItem>
+          ))}
+      </Grid>
+    </Box>
+  );
+}
