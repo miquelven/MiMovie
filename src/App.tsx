bae63305f3e6f973af445c61fd388a63e39ea624
miquelven <miquelven.silva@gmail.com>
2024-04-18 21:22:20 -0300

    feat: add auto scroll top when change page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <>
+      <ScrollToTop />
       <Header />
       <Outlet />
       <Footer />
