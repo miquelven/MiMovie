ecf7649712d706f163c29bba026e09356a0aab60
miquelven <miquelven.silva@gmail.com>
2024-04-18 21:25:19 -0300

    feat: add similarmoviesarea

---
+++ src/components/SimilarMoviesArea/index.tsx
@@ -0,0 +1,48 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/grid";
+import "swiper/css/pagination";
+
+import { Navigation, Pagination } from "swiper/modules";
+import { Box, Center, Heading } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+export default function SimilarMoviesArea({ similarMoviesData }) {
+  return (
+    <>
+      <Heading textAlign={"center"} mt="140px" mb="80px">
+        Filmes Semelhantes:
+      </Heading>
+      <Swiper
+        style={{ overflow: "hidden", height: "600px" }}
+        centeredSlides={true}
+        slidesPerView={3}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        data-swiperTrailer
+      >
+        {similarMoviesData.results.map((movie) => (
+          <SwiperSlide data-swiperSlideTrailer key={movie.id}>
+            <Link to={`/${movie.title.split(" ").join("-")}`}>
+              <Box>
+                <Center>
+                  <img
+                    src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
+                    alt={`Imagem do poster do filme ${movie.title}`}
+                    className="transition-all duration-500 w-[53%] object-cover cursor-pointer hover:brightness-50 "
+                  />
+                </Center>
+              </Box>
+            </Link>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
+  );
+}
