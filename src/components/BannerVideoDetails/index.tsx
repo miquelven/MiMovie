59f8703ac93e91cce2831fe74557cd49cf1ef821
miquelven <miquelven.silva@gmail.com>
2024-04-19 12:52:11 -0300

    feat: add skeleton

--- src/components/BannerVideoDetails/index.tsx
+++ src/components/BannerVideoDetails/index.tsx
@@ -4,7 +4,6 @@ import {
   Flex,
   Heading,
   IconButton,
-  Image,
   ListItem,
   Skeleton,
   Text,
@@ -14,11 +13,6 @@ import {
 import { FaExternalLinkAlt } from "react-icons/fa";
 import { CiHeart } from "react-icons/ci";
 // import { FaHeart } from "react-icons/fa";
-
-interface companieProp {
-  logo_path: string;
-  name: string;
-}
 
 interface genreProp {
   name: string;
@@ -35,7 +29,6 @@ interface propType {
     runtime: number;
     homepage: string;
     vote_average: number;
-    production_companies: companieProp[];
   };
   isLoading: boolean;
 }
@@ -66,31 +59,36 @@ export default function BannerVideoDetails({ data, isLoading }: propType) {
                 data!["backdrop_path"]
               })`
         }
-        backgroundPosition={["left", null, "center"]}
+        backgroundPosition={"center"}
         backgroundRepeat="no-repeat"
         position={"relative"}
         backgroundSize="cover"
-        height="70vh"
+        height={{ base: "100vh", sm: "60vh", xl: "75vh", "2xl": "70vh" }}
         width={"full"}
       >
         <div className="absolute inset-0 bg-black/80 z-10 "></div>
-        <Box position={"relative"} zIndex={20}></Box>
         <Box
           h="100%"
-          w="100%"
           position={"absolute"}
           zIndex={10}
-          left={"10%"}
-          mt="15%"
+          left={{ base: "0", xl: "10%" }}
+          mt={{ base: "30%", sm: "25%", md: "18%", lg: "24%", xl: "15%" }}
           maxW="1580px"
-          px={{ base: "30px", md: "10px" }}
         >
-          <Flex h="80%" gap="80px">
+          <Flex
+            h={{ base: "60%", sm: "70%", md: "80%" }}
+            gap={{ md: "30px", xl: "80px" }}
+            px={{ base: "30px", md: "10px" }}
+          >
             <Skeleton
-              w={500}
+              w={{ md: 300, xl: 500 }}
+              h={!isLoading ? "" : "100%"}
               isLoaded={!isLoading}
               startColor="#2d323f"
               endColor="#131722"
+              px={{ lg: "10px" }}
+              alignSelf={"center"}
+              className="hidden lg:block"
             >
               <img
                 src={`https://image.tmdb.org/t/p/w500${data?.poster_path}`}
@@ -98,50 +96,73 @@ export default function BannerVideoDetails({ data, isLoading }: propType) {
                 className="h-full"
               />
             </Skeleton>
-            <Flex flexDir="column" justifyContent={"space-between"} w="full">
-              <Flex gap="20px" flexDir="column">
-                <span className="text-2xl font-bold text-[#fffd]">
+            <Flex flexDir="column" justifyContent={"space-between"} w="100%">
+              <Flex gap={{ base: "40px", sm: "20px" }} flexDir="column">
+                <span className=" font-bold text-[#fffd] sm:text-xl 2xl:text-2xl">
                   <Skeleton
                     isLoaded={!isLoading}
                     startColor="#2d323f"
                     endColor="#131722"
                     width={"10%"}
                     height={isLoading ? 30 : ""}
+                    className="hidden sm:block"
                   >
                     {data?.release_date.split("-")[0]}
                   </Skeleton>
                 </span>
-                <Heading as="h1" fontSize={"6xl"}>
+                <Heading
+                  as="h1"
+                  fontSize={{ base: "3xl", sm: "5xl", xl: "6xl" }}
+                >
                   <Skeleton
                     isLoaded={!isLoading}
                     startColor="#2d323f"
                     endColor="#131722"
                     height={isLoading ? 50 : ""}
+                    width={isLoading ? "30%" : ""}
                   >
                     {data?.title}
                   </Skeleton>
                 </Heading>
-                <Text fontSize={"large"} lineHeight={"34px"} color="#fffd">
+                <Text
+                  fontSize={{ base: "smaller", sm: "medium", md: "large" }}
+                  lineHeight={{ base: "28px", xl: "34px" }}
+                  color="#fffd"
+                  w={"100%"}
+                >
                   <Skeleton
                     isLoaded={!isLoading}
                     startColor="#2d323f"
                     endColor="#131722"
                     height={isLoading ? "120px" : ""}
-                    width={isLoading ? "80%" : ""}
+                    width={
+                      !isLoading
+                        ? { sm: "100%", md: "95%" }
+                        : { base: "75vw", sm: "50vw" }
+                    }
                   >
                     {data?.overview}
                   </Skeleton>
                 </Text>
-                <Box mt="20px">
+                <Box
+                  mt={{ base: "20px", md: "2%" }}
+                  fontSize={{ base: "smaller", xl: "medium" }}
+                >
                   <Skeleton
                     isLoaded={!isLoading}
                     startColor="#2d323f"
                     endColor="#131722"
-                    width={isLoading ? "40%" : ""}
+                    width={isLoading ? "50%" : ""}
                     height={isLoading ? 10 : ""}
                   >
                     <UnorderedList
-                      style={{ display: "flex", gap: "40px", margin: "0" }}
+                      style={{
+                        display: "flex",
+                        flexWrap: "wrap",
+                        margin: "0",
+                      }}
+                      listStyleType={{ base: "none", md: "disc" }}
+                      gap={{ base: "20px", md: "40px" }}
                       color="#fffd"
                     >
                       <ListItem style={{ listStyle: "none" }}>
@@ -172,60 +193,74 @@ export default function BannerVideoDetails({ data, isLoading }: propType) {
                   </Skeleton>
                 </Box>
               </Flex>
-              <Box h="20%">
+              <Box h={{ base: "0", md: "40%", lg: "20%" }}>
                 <Skeleton
                   isLoaded={!isLoading}
                   startColor="#2d323f"
                   endColor="#131722"
-                  width={isLoading ? "100%" : ""}
-                  height={isLoading ? 50 : ""}
+                  width={isLoading ? "90%" : ""}
+                  height={isLoading ? 50 : "100%"}
+                  mt={!isLoading ? { base: "30px", sm: "0" } : { md: "70px" }}
                 >
                   <Flex
-                    alignItems="center"
-                    justifyContent={"space-between"}
+                    alignItems={{
+                      sm: "center",
+                      md: "flex-end",
+                      "2xl": "center",
+                    }}
                     h="100%"
-                    fontSize="xl"
+                    fontSize={{ sm: "lg", xl: "xl" }}
                     fontWeight={"bold"}
                     color="#fffd"
+                    flexDir={{ base: "column", md: "row" }}
                   >
-                    {data?.homepage && (
-                      <a href={data?.homepage} target="_blank">
-                        <Flex
-                          alignItems={"center"}
-                          gap="16px"
-                          color="#23a7d7"
-                          _hover={{ textDecor: "underline" }}
-                        >
-                          <span>Assistir o filme</span>
-                          <FaExternalLinkAlt fontSize={"20px"} />
-                        </Flex>
-                      </a>
+                    {data && (
+                      <Flex
+                        w="100%"
+                        align={"center"}
+                        justifyContent={{
+                          base: "space-between",
+                          md: "flex-start",
+                        }}
+                        gap={{ sm: "40px", md: "60px" }}
+                      >
+                        <a href={data.homepage} target="_blank">
+                          <Flex
+                            alignItems={{ base: "flex-start", xl: "center" }}
+                            gap="16px"
+                            color="#23a7d7"
+                            _hover={{ textDecor: "underline" }}
+                          >
+                            <span>Assistir o filme</span>
+                            <FaExternalLinkAlt className="sm:text-xl xl:text-xl" />
+                          </Flex>
+                        </a>
+                        <span className="sm:text-3xl xl:text-4xl">
+                          {data.vote_average.toFixed(1)}/10
+                        </span>
+                      </Flex>
                     )}
-                    <span className="text-4xl">
-                      {data?.vote_average.toFixed(1)}/10
-                    </span>
-
-                    <Box>
-                      <Image
-                        src={`https://image.tmdb.org/t/p/w300/${data?.production_companies[0].logo_path}`}
-                        alt={`Imagem da companhia '${data?.production_companies[0].name}'`}
-                        objectFit="cover"
-                        className="w-full h-full scale-50"
-                      />
-                    </Box>
-                    <Flex gap="40px">
+                    <Flex
+                      mt={{ base: "30px", sm: "20px", md: "0" }}
+                      gap={{ base: "10px", md: "40px" }}
+                      align={"center"}
+                      justifyContent={{
+                        base: "space-between",
+                        md: "flex-start",
+                      }}
+                      w={{ base: "100%", md: "auto" }}
+                    >
                       <Button
                         colorScheme="gray"
                         _hover={{ color: "#fff", background: "#ccc2" }}
-                        color="#000"
                       >
                         Assistir mais Tarde
                       </Button>
                       <Button
                         variant="ghost"
                         colorScheme="gray"
+                        px={{ base: 0, md: "10px" }}
                         _hover={{ background: "#ccc2" }}
-                        color="#fffd"
                       >
                         <IconButton
                           fontSize="4xl"
