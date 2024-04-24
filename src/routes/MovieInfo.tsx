d1856886493e8799d080ae23cf599442a931ccda
miquelven <miquelven.silva@gmail.com>
2024-04-23 23:56:27 -0300

    refactor: remove the imports components and hooks customized for optimize the code

--- src/routes/MovieInfo.tsx
+++ src/routes/MovieInfo.tsx
@@ -1,64 +1,49 @@
 import { Container } from "@chakra-ui/react";
-import useGetMoviesDetail from "../hooks/useGetMovieDetails";
 
-import { useLocalStorage } from "usehooks-ts";
-import { useEffect } from "react";
 import BannerVideoDetails from "../components/BannerVideoDetails";
 import CarouselVideosImages from "../components/CarouselVideosImages";
-import useGetTrailers from "../hooks/useGetTrailers";
-import useGetImages from "../hooks/useGetImages";
-import useGetReviews from "../hooks/useGetReviews";
 import ReviewsArea from "../components/ReviewsArea";
-import useGetCredits from "../hooks/useGetCredits";
 import CreditsArea from "../components/CreditsArea";
-import useGetSimilarMovies from "../hooks/useGetSimilarMovies";
 import SimilarMoviesArea from "../components/SimilarMoviesArea";
+import { useParams } from "react-router-dom";
+import useGetExamples from "../hooks/useGetExample";
 
 export default function MovieInfo() {
-  const [videoId, setVideoId] = useLocalStorage<number[]>("idsForTrailers", []);
-  const { data, isPending } = useGetMoviesDetail();
-
-  useEffect(() => {
-    if (!isPending && data !== undefined) setVideoId([data.id]);
-    console.log(videoId);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isPending]);
-
-  const { data: videosData, isPending: isPendingVideos } = useGetTrailers();
-  const { data: imagesData, isPending: isPendingImages } = useGetImages(
-    videoId[0]
-  );
-  const { data: reviewsData, isPending: isPendingReviews } = useGetReviews(
-    videoId[0]
-  );
-  const { data: creditsData, isPending: isPendingCredits } = useGetCredits(
-    videoId[0]
-  );
-  const { data: similarMoviesData, isPending: isPendingSimilar } =
-    useGetSimilarMovies(videoId[0]);
+  const { name } = useParams();
+  const { data: movieDetails, isPending: isLoading } = useGetExamples(name!);
 
   return (
     <main>
       <Container maxW="100vw" p={"0"} position="relative" mb="200px">
-        <BannerVideoDetails data={data} isLoading={isPending} />
-        <CarouselVideosImages
-          videoData={videosData!}
-          isLoadingVideo={isPendingVideos}
-          imagesData={imagesData}
-          isLoadingImage={isPendingImages}
+        <BannerVideoDetails
+          data={movieDetails?.reqBannerInfo.data}
+          isLoading={isLoading}
         />
-        <Container
-          mt="280px"
-          maxW="1580px"
-          px={{ base: "30px", md: "10px" }}
-          position="relative"
-        >
-          {!isPendingReviews && <ReviewsArea reviews={reviewsData!} />}
-          {!isPendingCredits && <CreditsArea credits={creditsData!} />}
-          {!isPendingSimilar && (
-            <SimilarMoviesArea similarMoviesData={similarMoviesData!} />
-          )}
-        </Container>
+        <CarouselVideosImages
+          videoData={movieDetails?.reqVideos.data}
+          imagesData={movieDetails?.reqImages.data}
+          isLoading={isLoading}
+        />
+        {!isLoading && (
+          <>
+            <Container
+              mt="280px"
+              maxW="1580px"
+              px={{ base: "30px", md: "10px" }}
+              position="relative"
+            >
+              {!isLoading && (
+                <ReviewsArea reviews={movieDetails?.reqReviews.data} />
+              )}
+              {!isLoading && (
+                <CreditsArea credits={movieDetails?.reqCredits.data} />
+              )}
+            </Container>
+            <SimilarMoviesArea
+              similarMoviesData={movieDetails?.reqSimilar.data}
+            />
+          </>
+        )}
       </Container>
     </main>
   );
