650a4f55ca588c8101f9db3714d625689c637fa0
miquelven <miquelven.silva@gmail.com>
2024-04-23 23:36:02 -0300

    style: add style in text

--- src/components/ReviewsArea/index.tsx
+++ src/components/ReviewsArea/index.tsx
@@ -18,31 +18,53 @@ interface propType {
 
 export default function ReviewsArea({ reviews }: propType) {
   return (
-    <Box mt="160px">
+    <Box>
       <Flex gap="80px" flexDir={"column"}>
         {reviews?.results.map(
           (review, index) =>
             index < 5 && (
               <Box
-                py="12px"
-                px="8px"
+                py={{ base: "32px", md: "12px" }}
+                px="12px"
+                key={index}
                 borderRadius={index % 2 == 0 ? "10px" : "0"}
                 bg={index % 2 == 0 ? "#1c212e" : ""}
+                position={"relative"}
               >
-                <Flex gap="40px">
+                <Flex
+                  gap={{ sm: "15px", lg: "40px" }}
+                  flexDir={{ sm: "column", md: "row" }}
+                >
                   <Avatar
-                    size="md"
+                    position={{ base: "absolute", md: "static" }}
+                    left={{ base: "-20px", sm: "-20px" }}
+                    top={{ base: "-20px", sm: "-30px" }}
+                    size={{ base: "md", sm: "lg", md: "md" }}
                     name={review.author}
                     src={review.author_details.avatar_path}
                   />
                   <Flex flexDir={"column"} gap="20px">
                     <Flex flexDir={"column"} gap="8px">
-                      <Heading as="h6">
+                      <Heading
+                        as="h6"
+                        fontSize={{ base: "2xl", sm: "3xl", lg: "4xl" }}
+                        textAlign={{ base: "center", md: "start" }}
+                      >
                         {review.author}
-                        <span> - {review.author_details.rating}/10</span>
+                        {review.author_details.rating && (
+                          <span> - {review.author_details.rating}/10</span>
+                        )}
                       </Heading>
 
-                      <Text color="#fffd">
+                      <Text
+                        color="#fffd"
+                        fontSize={{
+                          base: "small",
+                          sm: "smaller",
+                          lg: "medium",
+                        }}
+                        textAlign={{ base: "center", lg: "start" }}
+                      >
                         <span>
                           {review.created_at.split("T")[0].split("-")[2]}-
                         </span>
@@ -54,7 +76,13 @@ export default function ReviewsArea({ reviews }: propType) {
                         </span>
                       </Text>
                     </Flex>
-                    <Text color="#fffd" lineHeight={"32px"} fontSize={"lg"}>
+                    <Text
+                      color="#fffd"
+                      lineHeight={{ base: "24px", md: "32px" }}
+                      fontSize={{ base: "smaller", sm: "medium", lg: "large" }}
+                      textAlign={{ base: "justify", md: "start" }}
+                      px={{ base: "10px", md: "0" }}
+                    >
                       {review.content}
                     </Text>
                   </Flex>
