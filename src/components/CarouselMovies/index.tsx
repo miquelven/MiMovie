2488a5f1454d184479520f8d81799f4748480717
miquelven <miquelven.silva@gmail.com>
2024-04-23 23:25:49 -0300

    feat: replace the content with a component

--- src/components/CarouselMovies/index.tsx
+++ src/components/CarouselMovies/index.tsx
@@ -10,17 +10,13 @@ import "./style.css";
 // import required modules
 import { Pagination } from "swiper/modules";
 import useGetMovies from "../../hooks/useGetMovies";
-import { AspectRatio, Box, Heading, Image, Text } from "@chakra-ui/react";
-import useGetGenres from "../../hooks/useGetGenres";
+
+import LatestMoviesCard from "../LatestMoviesCard";
 
 interface movieType {
   id: number;
   backdrop_path: string;
   title: string;
-}
-
-interface genreType {
-  name: string;
 }
 
 export default function CarouselMovies() {
@@ -29,10 +25,10 @@ export default function CarouselMovies() {
     1
   );
 
-  const { data: genresData, isPending: isPendingGenres } = useGetGenres();
   return (
     <>
       <Swiper
+        slidesPerView={2}
         breakpoints={{
           640: {
             slidesPerView: 2,
@@ -59,55 +55,8 @@ export default function CarouselMovies() {
           data &&
           data.results &&
           data.results.map((movie: movieType, index: number) => (
-            <SwiperSlide data-swiperSlideMovie key={movie.id}>
-              <AspectRatio className="h-[300px] relative" ratio={4 / 3}>
-                <>
-                  <Image
-                    src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
-                    alt="naruto"
-                    objectFit="cover"
-                  />
-                  <div className="transition-all duration-300 absolute inset-0 bg-black/60 hover:bg-black/30 ">
-                    <div className="absolute bottom-10 left-0 w-full flex flex-col px-3">
-                      <Heading textAlign={"center"} as="h4" fontSize={"22px"}>
-                        {movie.title}
-                      </Heading>
-                      <Box mt="24px" height={"40px"}>
-                        <div className="max-w-[75%] mx-auto grid grid-cols-2 gap-1 justify-center items-center max-sm:grid-cols-1">
-                          {/* categories */}
-                          {!isPendingGenres && genresData && (
-                            <>
-                              {genresData[index].length > 1 ? (
-                                genresData[index].map(
-                                  (genre: genreType, genreIndex: number) => (
-                                    <Text
-                                      fontWeight={"light"}
-                                      textAlign={"center"}
-                                      fontSize={"12px"}
-                                      key={genreIndex}
-                                    >
-                                      {genre.name}
-                                    </Text>
-                                  )
-                                )
-                              ) : (
-                                <Text
-                                  fontWeight={"light"}
-                                  textAlign={"center"}
-                                  fontSize={"12px"}
-                                  className="col-span-2"
-                                >
-                                  {genresData[index].name}
-                                </Text>
-                              )}
-                            </>
-                          )}
-                        </div>
-                      </Box>
-                    </div>
-                  </div>
-                </>
-              </AspectRatio>
+            <SwiperSlide data-swiperSlideMovie key={index}>
+              <LatestMoviesCard data={movie} />
             </SwiperSlide>
           ))}
       </Swiper>
