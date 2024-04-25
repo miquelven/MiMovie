4aa6ddc8b84ff82950c09b55dcad111445cd661a
miquelven <miquelven.silva@gmail.com>
2024-04-24 23:08:52 -0300

    style: change font size

--- src/components/PaginationArea/index.tsx
+++ src/components/PaginationArea/index.tsx
@@ -14,7 +14,7 @@ export default function PaginationArea({ infos }: infosType) {
   return (
     <Box position={"absolute"} bottom={"-80px"} left="50%" right="50%">
       <Center>
-        <Flex gap="24px" fontSize={{ base: "medium", sm: "large" }}>
+        <Flex fontSize={{ base: "medium", sm: "large" }}>
           {infos.pageSelected > 1 && (
             <>
               <PaginationButton infos={{ ...infos, type: "prev" }}>
