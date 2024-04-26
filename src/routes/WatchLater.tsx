86dd723d39779a42dd636aa49a4d25fdfb969a70
miquelven <miquelven.silva@gmail.com>
2024-04-25 23:00:26 -0300

    feat: build watchlater page

--- src/routes/WatchLater.tsx
+++ src/routes/WatchLater.tsx
@@ -1,13 +1,38 @@
+import { Box, Container, Grid } from "@chakra-ui/react";
 import { useWatchLaterStore } from "../stores/watchLaterStore";
+import WatchLaterItem from "../components/WatchLaterItem";
+import TitleDescription from "../components/TitleDescription";
 
 export default function WatchLater() {
   const watchLaterMovie = useWatchLaterStore((state) => state.watchLaterMovie);
 
   return (
-    <div>
-      {watchLaterMovie.map((item) => (
-        <div>{item.title}</div>
-      ))}
-    </div>
+    <main>
+      <Container
+        maxW="1580px"
+        px={{ base: "30px", md: "10px" }}
+        mt={{ base: "170px", sm: "200px" }}
+        mb="150px"
+      >
+        <Box>
+          <TitleDescription
+            title="Filmes para Assistir Mais Tarde"
+            description="Explore sua lista pessoal de filmes para assistir mais tarde."
+          />
+          <Box minH={"400px"} mt={{ base: "40px", md: "80px" }}>
+            <Grid
+              bg="#0a0d14"
+              p="20px"
+              templateColumns={{ base: "repeat(1,1fr)", md: "repeat(2,1fr)" }}
+              rowGap={{ base: "18px", md: "28px" }}
+            >
+              {watchLaterMovie.map((item, index) => (
+                <WatchLaterItem data={item} key={index} />
+              ))}
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </main>
   );
 }
