c7a9515f1096a28a58ec96b890eaaf1427699e88
miquelven <miquelven.silva@gmail.com>
2024-04-25 22:58:40 -0300

    feat: add typing and add skeleton

--- src/components/TopList/TopListMovieInfo/index.tsx
+++ src/components/TopList/TopListMovieInfo/index.tsx
@@ -6,6 +6,7 @@ import {
   Grid,
   Heading,
   Hide,
+  Skeleton,
   Text,
 } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
@@ -21,7 +22,11 @@ interface currentItemProp {
   release_date: string;
 }
 
-export default function TopListMovieInfo() {
+interface propType {
+  isLoading: boolean;
+}
+
+export default function TopListMovieInfo({ isLoading }: propType) {
   const [currentItem] = useLocalStorage<currentItemProp | null>(
     "weekCurrentItem",
     null
@@ -32,30 +37,35 @@ export default function TopListMovieInfo() {
   return (
     <Hide below="lg">
       <Flex flex="2" flexDir={"column"}>
-        {currentItem && (
-          <>
-            <Flex gap="40px" position={"relative"} maxH="400px">
+        <>
+          <Skeleton
+            isLoaded={!isLoading}
+            startColor="#2d323f"
+            endColor="#131722"
+            maxH="400px"
+          >
+            <Flex gap="40px" position={"relative"}>
               <img
-                src={`https://image.tmdb.org/t/p/original${currentItem.poster_path}`}
-                alt={`Imagem do filme ${currentItem.title}`}
+                src={`https://image.tmdb.org/t/p/original${currentItem?.poster_path}`}
+                alt={`Imagem do filme ${currentItem?.title}`}
                 className="max-w-[200px] w-10/12 object-cover"
               />
               <Center>
                 <Flex flexDir="column" gap="32px">
                   <Box>
                     <Text fontSize={"14px"} color="#fff8">
-                      {currentItem.release_date.split("-")[0]}
+                      {currentItem?.release_date.split("-")[0]}
                     </Text>
                     <Heading as="h3" fontSize={{ md: "2xl", xl: "4xl" }}>
-                      {currentItem.title}
+                      {currentItem?.title}
                     </Heading>
                   </Box>
                   <Text noOfLines={3} fontSize={{ md: "sm", xl: "medium" }}>
-                    {currentItem.overview}
+                    {currentItem?.overview}
                   </Text>
                   <Link
-                    onClick={() => setCurrentMovie(currentItem.id)}
-                    to={`/${currentItem.title.split(" ").join("-")}`}
+                    onClick={() => setCurrentMovie(currentItem!.id)}
+                    to={`/${currentItem?.title.split(" ").join("-")}`}
                     className="text-sm md:text-sm xl:text-base text-[#23a7d7] hover:text-[#005282] hover:underline"
                   >
                     Ver Mais Informações
@@ -64,12 +74,15 @@ export default function TopListMovieInfo() {
               </Center>
             </Flex>
             <Divider mt="24px" borderColor="#fff4" />
-            <Grid
-              mt="40px"
-              h="100%"
-              templateColumns={"repeat(2, 1fr)"}
-              gap="40px"
-            >
+          </Skeleton>
+          <Skeleton
+            isLoaded={!isLoading}
+            startColor="#2d323f"
+            endColor="#131722"
+            mt="40px"
+            h="100%"
+          >
+            <Grid templateColumns={"repeat(2, 1fr)"} gap="40px">
               {gridItems.length > 0 &&
                 gridItems.map(
                   (item, index) =>
@@ -78,8 +91,8 @@ export default function TopListMovieInfo() {
                     )
                 )}
             </Grid>
-          </>
-        )}
+          </Skeleton>
+        </>
       </Flex>
     </Hide>
   );
