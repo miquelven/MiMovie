fe22a813ef75038d5f1852e48b954c8308307f02
miquelven <miquelven.silva@gmail.com>
2024-04-26 19:42:00 -0300

    feat: add search component

---
+++ src/routes/Search.tsx
@@ -0,0 +1,24 @@
+import { Container } from "@chakra-ui/react";
+import ListMovies from "../components/ListMovies";
+import { useParams } from "react-router-dom";
+
+export default function Search() {
+  const params = useParams();
+
+  return (
+    <main>
+      <Container
+        maxW="1580px"
+        px={{ base: "30px", md: "10px" }}
+        mt={{ base: "170px", sm: "200px" }}
+        mb="150px"
+      >
+        <ListMovies
+          title={`Resultados da pesquisa: ${params.name}`}
+          desc="O lugar onde você encontra todos os resultados da sua pesquisa."
+          url={`search/movie?query=${params.name}&language=pt-BR&page=`}
+        />
+      </Container>
+    </main>
+  );
+}
