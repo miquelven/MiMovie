07714d35ab57a682904c29e41b886be4e0537668
miquelven <miquelven.silva@gmail.com>
2024-04-27 21:47:19 -0300

    refactor: improves the way you search for data

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -107,95 +107,94 @@ export default function Banner() {
             draggable
             autoplaySpeed={4000}
           >
-            {data?.results &&
-              [0, 1, 2].map((vIndex) => (
-                <>
-                  <Box
-                    height={"6xl"}
-                    background={`url(https://image.tmdb.org/t/p/original${data.results[vIndex]["backdrop_path"]})`}
-                    backgroundPosition={["left", null, "center"]}
-                    backgroundRepeat="no-repeat"
-                    backgroundSize="cover"
-                    className="hover:cursor-grab hover:active:cursor-grabbing"
-                    key={vIndex}
-                  >
-                    {/* opacity */}
-                    <div className="absolute inset-0 bg-black/20 z-10 "></div>
-                    <div className="absolute h-[5%] w-full top-[0] bg-gradient-to-b from-[#0a0d1445] via-white/0 to-white/0 z-20"></div>
-                    <div className="absolute h-[20%] w-full bottom-0 bg-gradient-to-t  from-[#0a0d14] via-[#0a0d14] to-white/0 z-50"></div>
-
-                    <Container
-                      height="100vh"
-                      px="0px !important"
-                      maxWidth={["300px", "700px", null, null, "1000px"]}
-                      position="relative"
-                      style={{ zIndex: 20 }}
-                    >
-                      <Stack
-                        height="400px"
-                        width="100%"
-                        position="absolute"
-                        top="50%"
-                        transform="translate(0, -50%)"
+            {data &&
+              data.results.map(
+                (movieInfo, index) =>
+                  index < 3 && (
+                    <>
+                      <Box
+                        height={"6xl"}
+                        background={`url(https://image.tmdb.org/t/p/original${movieInfo["backdrop_path"]})`}
+                        backgroundPosition={["left", null, "center"]}
+                        backgroundRepeat="no-repeat"
+                        backgroundSize="cover"
+                        className="hover:cursor-grab hover:active:cursor-grabbing"
+                        key={index}
                       >
-                        <Flex flexDir="column" gap="40px">
-                          <Heading
-                            textAlign="center"
-                            fontSize={{ base: "3xl", md: "4xl", lg: "5xl" }}
-                            color="#eee"
-                            mb="20px"
-                          >
-                            {data.results[vIndex].title}
-                          </Heading>
-                          <Text
-                            lineHeight={"32px"}
-                            fontSize={{ base: "sm", xl: "lg" }}
-                            width={{ base: "100%", sm: "80%" }}
-                            mx="auto"
-                            textAlign="center"
-                            noOfLines={4}
-                            color="#cfcfcf"
-                          >
-                            {data.results[vIndex].overview}
-                          </Text>
-                        </Flex>
-                      </Stack>
+                        {/* opacity */}
+                        <div className="absolute inset-0 bg-black/20 z-10 "></div>
+                        <div className="absolute h-[5%] w-full top-[0] bg-gradient-to-b from-[#0a0d1445] via-white/0 to-white/0 z-20"></div>
+                        <div className="absolute h-[20%] w-full bottom-0 bg-gradient-to-t  from-[#0a0d14] via-[#0a0d14] to-white/0 z-50"></div>
 
-                      <Center
-                        style={{ zIndex: 20 }}
-                        position="absolute"
-                        right="0"
-                        left="0"
-                        bottom="0"
-                      >
-                        <Flex
-                          flexDir={"column"}
-                          alignItems={"center"}
-                          position={"relative"}
-                          style={{ zIndex: 30 }}
-                          height="100%"
-                          paddingBottom={{ base: "150px", sm: "80px" }}
+                        <Container
+                          height="100vh"
+                          px="0px !important"
+                          maxWidth={["300px", "700px", null, null, "1000px"]}
+                          position="relative"
+                          style={{ zIndex: 20 }}
                         >
-                          <Link
-                            onClick={() =>
-                              setCurrentMovie(data.results[vIndex].id)
-                            }
-                            to={`/${data.results[vIndex].title
-                              .split(" ")
-                              .join("-")}`}
-                            className="text-sm sm:text-base mb-5 text-[#cfcfcf] hover:text-[#eee] hover:underline"
+                          <Stack
+                            height="400px"
+                            width="100%"
+                            position="absolute"
+                            top="50%"
+                            transform="translate(0, -50%)"
                           >
-                            Ver Mais Informações
-                          </Link>
-                          <Box opacity={{ base: 0, md: 1 }}>
-                            <ArrowRightIcon style={{ rotate: "90deg" }} />
-                          </Box>
-                        </Flex>
-                      </Center>
-                    </Container>
-                  </Box>
-                </>
-              ))}
+                            <Flex flexDir="column" gap="40px">
+                              <Heading
+                                textAlign="center"
+                                fontSize={{ base: "3xl", md: "4xl", lg: "5xl" }}
+                                color="#eee"
+                                mb="20px"
+                              >
+                                {movieInfo.title}
+                              </Heading>
+                              <Text
+                                lineHeight={"32px"}
+                                fontSize={{ base: "sm", xl: "lg" }}
+                                width={{ base: "100%", sm: "80%" }}
+                                mx="auto"
+                                textAlign="center"
+                                noOfLines={4}
+                                color="#cfcfcf"
+                              >
+                                {movieInfo.overview}
+                              </Text>
+                            </Flex>
+                          </Stack>
+
+                          <Center
+                            style={{ zIndex: 20 }}
+                            position="absolute"
+                            right="0"
+                            left="0"
+                            bottom="0"
+                          >
+                            <Flex
+                              flexDir={"column"}
+                              alignItems={"center"}
+                              position={"relative"}
+                              style={{ zIndex: 30 }}
+                              height="100%"
+                              paddingBottom={{ base: "150px", sm: "80px" }}
+                            >
+                              <Link
+                                onClick={() => setCurrentMovie(movieInfo.id)}
+                                to={`/${movieInfo.title.split(" ").join("-")}`}
+                                className="text-sm sm:text-base mb-5 text-[#cfcfcf] hover:text-[#eee] hover:underline"
+                              >
+                                Ver Mais Informações
+                              </Link>
+                              <Box opacity={{ base: 0, md: 1 }}>
+                                <ArrowRightIcon style={{ rotate: "90deg" }} />
+                              </Box>
+                            </Flex>
+                          </Center>
+                        </Container>
+                      </Box>
+                    </>
+                  )
+              )}
           </Slider>
         </Box>
       </Container>
