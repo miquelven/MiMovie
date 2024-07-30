6935ef43f0bf6e87c271e273dec93bfe93b30a74
miquelven <miquelven.silva@gmail.com>
2024-07-30 18:02:43 -0300

    feat: add message if no movie found

--- src/components/ListMovies/index.tsx
+++ src/components/ListMovies/index.tsx
@@ -22,7 +22,7 @@ export default function ListMovies({ title, desc, url }: propType) {
   useEffect(() => setCurrentPage(1), [url]);
 
   return (
-    <section className="min-h-[calc(100vh+550px)] relative">
+    <section className="min-h-screen relative">
       {isPending && (
         <div className="fixed inset-0 bg-black/70 flex z-20 justify-center items-center scale-150">
           <Spinner
@@ -35,25 +35,31 @@ export default function ListMovies({ title, desc, url }: propType) {
         </div>
       )}
       <TitleDescription title={title} description={desc} />
-      <Grid
-        templateColumns={{
-          base: "repeat(2, 1fr)",
-          sm: "repeat(3, 1fr)",
-          md: "repeat(4, 1fr)",
-          lg: "repeat(5, 1fr)",
-        }}
-        gap={{ sm: "20px", lg: "40px" }}
-        rowGap={"80px"}
-        mt="80px"
-      >
-        {!isPending &&
-          data?.results.map(
-            (item, index: number) =>
-              index < 12 && <CardMovie key={index} data={item} />
-          )}
-      </Grid>
+      {!isPending && data && data?.results.length > 0 ? (
+        <Grid
+          templateColumns={{
+            base: "repeat(2, 1fr)",
+            sm: "repeat(3, 1fr)",
+            md: "repeat(4, 1fr)",
+            lg: "repeat(5, 1fr)",
+          }}
+          gap={{ sm: "20px", lg: "40px" }}
+          rowGap={"80px"}
+          mt="80px"
+        >
+          {!isPending &&
+            data?.results.map(
+              (item, index: number) =>
+                index < 12 && <CardMovie key={index} data={item} />
+            )}
+        </Grid>
+      ) : (
+        <span className="w-full text-center text-4xl font-medium block mt-60 text-white/70 max-sm:text-xl max-sm:mt-40">
+          Nenhum filme encontrado!
+        </span>
+      )}
 
-      {!isPending && data && (
+      {!isPending && data && data?.results.length > 0 && (
         <PaginationArea
           infos={{
             setPage,
