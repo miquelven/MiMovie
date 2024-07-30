8ad1cad3f1aa5900cc45688983096b7bd6427634
miquelven <miquelven.silva@gmail.com>
2024-07-30 17:45:14 -0300

    feat: add config react toastify

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,9 @@ import { ChakraProvider } from "@chakra-ui/react";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 // react router
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./routes/Home.tsx";
@@ -51,6 +54,11 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider>
       <QueryClientProvider client={client}>
+        <ToastContainer
+          toastStyle={{ background: "#0a0d14", color: "white" }}
+          icon={false}
+          hideProgressBar={true}
+        />
         <RouterProvider router={router} />
       </QueryClientProvider>
     </ChakraProvider>
